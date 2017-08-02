ce9330aa439c3aa0eca8b0795162e7402cc1199e
Brian Wilbur <Brian.Wilbur@dep.nj.gov>
2017-08-02 13:28:49 -0400

    Router outlet container, ready to go i think

--- app/home/home.component.ts
+++ app/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-	selector: 'sequoia',
+	selector: 'mean-skeleton-home',
 	styles: [``],
 	template: `<h1>MEAN Skeleton Application</h1>`
 })
